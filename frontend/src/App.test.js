d952197f6bb1142ee2e0eb55972c3326914820dd
Talha <talha0pse@gmail.com>
2025-05-16 08:03:39 +0530

    updated App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders login heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headings = screen.getAllByText(/login/i);
+  // Check if at least one "Login" heading is present
+  expect(headings[0]).toBeInTheDocument();
 });
